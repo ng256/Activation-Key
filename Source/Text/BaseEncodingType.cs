cfcc98a275e3b2733cf7257bfff9183798f79bb6
Pavel Bashkardin <90511962+ng256@users.noreply.github.com>
2024-05-07 09:15:14 +0300

    Add files via upload

---
+++ Source/Text/BaseEncodingType.cs
@@ -0,0 +1,44 @@
+﻿/***************************************************************
+
+•   File: BaseEncodingTypes.cs
+
+•   Description.
+
+    Represents      an    enum        of     encoding     types.
+    Each   enumeration  element   represents a specific encoding
+    type.
+
+    This can be useful when working with various data types that
+    require a specific encoding.
+
+ ***************************************************************/
+
+namespace System.Text
+{
+    /// <summary>
+    /// Defines encoding types.
+    /// </summary>
+    public enum BaseEncodingType
+    {
+
+        /// <summary>
+        /// Represents the decimal number system.
+        /// </summary>
+        Base10,
+
+        /// <summary>
+        /// Represents the hexadecimal encoding.
+        /// </summary>
+        Base16,
+
+        /// <summary>
+        /// Represents a 32 character encoding.
+        /// </summary>
+        Base32,
+
+        /// <summary>
+        /// Represents a 64 character encoding.
+        /// </summary>
+        Base64
+    }
+}
